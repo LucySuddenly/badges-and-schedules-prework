d307a437f27c7c4e7b8204d7d9eb14ed9e50b4f3
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-15 16:09:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,7 @@ def printer(array)
   array.each do |item|
     name = []
     name << item
-    puts batch_badge_creator(name)
-    puts assign_rooms(name)
+    puts batch_badge_creator(name).reduce(0)
+    puts assign_rooms(name).reduce(0)
   end 
 end 
